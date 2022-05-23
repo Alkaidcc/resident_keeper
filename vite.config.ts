dde52f1611e26b1f940aec0574e2dda97a1e8529
Alkaidcc <932214806@qq.com>
2022-05-23 19:13:18 +0800

    :wrench: modify config

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-    }
+    },
   },
   plugins: [vue()],
   server: {
     port: 8080,
     hmr: {
-      host: "localhost",
-      port: 8080
-    }
+      host: 'localhost',
+      port: 8080,
+    },
     //proxy
-  }
+  },
 })
